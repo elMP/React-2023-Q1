3de6a3ba3a48afb16de6d2e662ed72a013c1b679
kneazle <kneazle@yandex.ru>
2023-03-19 00:04:29 +0300

    feat: add pages and router

--- rss-react-course/src/App.tsx
+++ rss-react-course/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+import About from './pages/About'
+import MainPage from './pages/MainPage'
+import NotFound from './pages/NotFound'
 
 import './App.css'
+import HomePage from 'pages/MainPage'
 
 function App() {
   return (
-    <div>
-      <p>
-        Hello
-      </p>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="about" element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
